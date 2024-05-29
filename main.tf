fdd88b2c2236b9ec0cb8495b863ec60a980df3f4
Rubeenakhan16 <142878059+Rubeenakhan16@users.noreply.github.com>
2024-05-29 17:07:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,22 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
 resource "aws_ecs_cluster" "example" {
-  name = "example"
+  name = "example-cluster"
 }
 
 resource "aws_ecs_task_definition" "example" {
-  family                   = "example"
+  family                   = "example-task"
   container_definitions    = jsonencode([{
     name      = "example"
     image     = "amazon/amazon-ecs-sample"
@@ -18,9 +28,31 @@ resource "aws_ecs_task_definition" "example" {
 }
 
 resource "aws_ecs_service" "example" {
-  name            = "example"
+  name            = "example-service"
   cluster         = aws_ecs_cluster.example.id
   task_definition = aws_ecs_task_definition.example.arn
   desired_count   = 1
-  launch_type     = "EC2"
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = [aws_subnet.example.id]
+    security_groups = [aws_security_group.example.id]
+  }
+}
+
+resource "aws_security_group" "example" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
