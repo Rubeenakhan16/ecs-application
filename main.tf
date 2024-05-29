450c25571a048aa42233c3a107b05c74ad3a8b5c
Rubeenakhan16 <142878059+Rubeenakhan16@users.noreply.github.com>
2024-05-29 17:51:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,11 +38,3 @@ resource "aws_ecs_service" "example" {
     security_groups = ["sg-0188ea9c19e25dad7"] // Replace with your default security group ID
   }
 }
-
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.example.id
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.example.name
-}
